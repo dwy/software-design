ab50aee68b9d896b2ef810e04c32ddbc5e8ea097
dominic wyler <dominic.wyler@gmail.com>
2014-11-05 18:27:56 +0100

    introduced nunit and added new class for nunit tests

--- strategy/Person/Person/PersonTestMsTest.cs
+++ strategy/Person/Person/PersonTestMsTest.cs
@@ -1,44 +1,43 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Person
 {
-	[TestFixture]
-	public class PersonTest
+	[TestClass]
+	public class PersonTestMsTest
 	{
-		[Test]
+		[TestMethod]
 		public void ToString_defaultMode()
 		{
 			Person simon = new Person("Ammann", "Simon", "CH");
 			Assert.AreEqual("Simon Ammann", simon.ToString());
 		}
 
-		[Test]
+		[TestMethod]
 		public void ToString_olympicModeAndCapitalize()
 		{
 			Person simon = new Person("Ammann", "Simon", "CH", true, true);
 			Assert.AreEqual("Simon AMMANN", simon.ToString());
 		}
 
-		[Test]
+		[TestMethod]
 		public void ToString_chineseInNonOlympicNonCapitalize()
 		{
 			Person yao = new Person("Yao", "Ming", "CHN", false, false);
 			Assert.AreEqual("Ming Yao", yao.ToString());
 		}
 
-		[Test]
+		[TestMethod]
 		public void ToString_chineseInOlympicMode()
 		{
 			Person yao = new Person("Yao", "Ming", "CHN", true, false);
 			Assert.AreEqual("Yao Ming", yao.ToString());
 		}
 
-		[Test]
+		[TestMethod]
 		public void ToString_chineseInOlympicModeAndCapitalize()
 		{
 			Person yao = new Person("Yao", "Ming", "CHN", true, true);
 			Assert.AreEqual("YAO Ming", yao.ToString());
 		}
-
 	}
 }
