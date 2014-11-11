941ac2c7c8cc0f09cce1c6bf2a57c53d5a25a205
dominic wyler <dominic.wyler@gmail.com>
2014-11-11 08:03:41 +0100

    extracted strategy factory

--- strategy/Person/Person/Person.cs
+++ strategy/Person/Person/Person.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Person
 {
 	public class Person
 	{
-		private String familyName;
-		private String givenName;
+		private readonly String familyName;
+		private readonly String givenName;
 		private String nationality;
 		private bool capitalizeSurname;
 		private bool olympicMode;
 
 		private readonly PersonNameStrategy _strategy;
-		public static readonly List<String> SurnameFirst = new List<String> {"CHN", "KOR"};
 
 
 		public Person(String familyName, String givenName, String nationality,
@@ -23,27 +21,12 @@ namespace Person
 			this.nationality = nationality;
 			this.capitalizeSurname = capitalizeSurname;
 			this.olympicMode = olympicMode;
-			_strategy = CreateStrategy(this.nationality, this.capitalizeSurname, this.olympicMode);
-		}
-
-		public static PersonNameStrategy CreateStrategy(string nationality, bool capitalizeSurname, bool olympicMode)
-		{
-			if (olympicMode)
-			{
-				return new OlympicPersonNameStrategy(capitalizeSurname, IsSurnameFirst(nationality));
-			}
-
-			return new DefaultPersonNameStrategy(capitalizeSurname);
+			_strategy = PersonNameStrategyFactory.Create(this.nationality, this.capitalizeSurname, this.olympicMode);
 		}
 
 		public override string ToString()
 		{
 			return _strategy.NameString(givenName, familyName);
-		}
-
-		public static bool IsSurnameFirst(string nationality)
-		{
-			return SurnameFirst.Contains(nationality);
 		}
 	}
 }
---
+++ strategy/Person/Person/PersonNameStrategyFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Person
+{
+	public static class PersonNameStrategyFactory
+	{
+		private static readonly List<String> SurnameFirst = new List<String> {"CHN", "KOR"};
+
+		public static PersonNameStrategy Create(string nationality, bool capitalizeSurname, bool olympicMode)
+		{
+			if (olympicMode)
+			{
+				return new OlympicPersonNameStrategy(capitalizeSurname, IsSurnameFirst(nationality));
+			}
+
+			return new DefaultPersonNameStrategy(capitalizeSurname);
+		}
+
+		private static bool IsSurnameFirst(string nationality)
+		{
+			return SurnameFirst.Contains(nationality);
+		}
+	}
+}
