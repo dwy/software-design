0dd00b5938b96df590446194267790b28dae3436
dominic wyler <dominic.wyler@gmail.com>
2014-12-03 18:13:52 +0100

    added number sequence

--- FizzBuzz/FizzBuzz/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using System.Text;
+using NUnit.Framework;
 
 namespace FizzBuzz
 {
@@ -13,9 +15,19 @@ namespace FizzBuzz
 			Assert.AreEqual("1", numbers);
 		}
 
+		[Test]
+		public void NormalNumberSequence_NumbersArePrinted()
+		{
+			string numbers = FizzBuzz(2);
+
+			Assert.AreEqual("1, 2", numbers);
+		}
+
+
 		private static string FizzBuzz(int count)
 		{
-			return "" + count;
+			var numbers = Enumerable.Range(1, count);
+			return string.Join(", ", numbers);
 		}
 	}
 }
