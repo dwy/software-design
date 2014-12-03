9e8246c8777f9ff32ad6e3fb496b96b888d79c00
dominic wyler <dominic.wyler@gmail.com>
2014-12-03 18:29:13 +0100

    handle Fizz Buzz case

--- FizzBuzz/FizzBuzz/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzz/FizzBuzzTest.cs
@@ -40,6 +40,14 @@ namespace FizzBuzz
 			Assert.AreEqual("1, 2, Fizz, 4, Buzz", numbers);
 		}
 
+		[Test]
+		public void MultipleOfFifteen_DisplaysFizzBuzz()
+		{
+			string numbers = FizzBuzz(15);
+
+			Assert.AreEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz", numbers);
+		}
+
 		private static string FizzBuzz(int count)
 		{
 			IEnumerable<string> numbers = Enumerable.Range(1, count).Select(GetNumberString);
@@ -48,6 +56,10 @@ namespace FizzBuzz
 
 		private static string GetNumberString(int n)
 		{
+			if (n % 3 == 0 && n % 5 == 0)
+			{
+				return "Fizz Buzz";
+			}
 			if (n % 3 == 0)
 			{
 				return "Fizz";
