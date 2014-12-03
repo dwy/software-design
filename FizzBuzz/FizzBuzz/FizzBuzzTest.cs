c84fdd878872ffefeae1f0f00b39954a55d2d4f4
dominic wyler <dominic.wyler@gmail.com>
2014-12-03 08:21:18 +0100

    added initial project for fizzbuzz

---
+++ FizzBuzz/FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz
+{
+	[TestFixture]
+	public class FizzBuzzTest
+	{
+		[Test]
+		public void SingleNormalNumber_NumberIsPrinted()
+		{
+			string numbers = FizzBuzz(1);
+
+			Assert.AreEqual("1", numbers);
+		}
+
+		private static string FizzBuzz(int count)
+		{
+			return "" + count;
+		}
+	}
+}
