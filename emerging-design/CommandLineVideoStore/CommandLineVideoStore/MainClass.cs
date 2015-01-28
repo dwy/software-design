cb69a88eabfd8f466571742d4909efc0b11bf4b7
dominic wyler <dominic.wyler@gmail.com>
2015-01-28 18:39:35 +0100

    added project for exercise on procedural code

---
+++ emerging-design/CommandLineVideoStore/CommandLineVideoStore/MainClass.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CommandLineVideoStore
+{
+    public class MainClass
+    {
+        private readonly TextReader _in;
+        private readonly TextWriter _out;
+
+        public static void Main()
+        {
+            new MainClass(Console.In, Console.Out).Run();
+            Console.ReadLine();
+        }
+
+        public MainClass(TextReader @in, TextWriter @out)
+        {
+            _out = @out;
+            _in = @in;
+        }
+
+        public void Run()
+        {
+            // read movies from file
+            var movies = new List<string[]>();
+            using (FileStream fs = File.Open(@"movies.cvs", FileMode.Open, FileAccess.Read))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader reader = new StreamReader(bs))
+            {
+                int movieNumber = 0;
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] movie = line.Split(';');
+                    movies.Add(movie);
+                    _out.WriteLine(movieNumber + ": " + movie[0]);
+                    movieNumber++;
+                }
+            }
+
+            _out.Write("Enter customer name: ");
+            string customerName = _in.ReadLine();
+
+            _out.WriteLine("Choose movie by number followed by rental days, just ENTER for bill:");
+
+            decimal totalAmount = 0;
+            int frequentRenterPoints = 0;
+            string result = "Rental Record for " + customerName + "\n";
+            while (true)
+            {
+                string input = _in.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+                string[] rental = input.Split(' ');
+                string[] movie = movies[int.Parse(rental[0])];
+                decimal thisAmount = 0;
+
+                int daysRented = int.Parse(rental[1]);
+                //determine amounts for rental
+                switch (movie[1])
+                {
+                    case "REGULAR":
+                        thisAmount += 2;
+                        if (daysRented > 2)
+                            thisAmount += (daysRented - 2)*1.5m;
+                        break;
+                    case "NEW_RELEASE":
+                        thisAmount += daysRented*3;
+                        break;
+                    case "CHILDRENS":
+                        thisAmount += 1.5m;
+                        if (daysRented > 3)
+                            thisAmount += (daysRented - 3)*1.5m;
+                        break;
+                }
+
+                // add frequent renter points
+                frequentRenterPoints++;
+                // add bonus for a two day new release rental
+                if (movie[1].Equals("NEW_RELEASE") && daysRented > 1)
+                {
+                    frequentRenterPoints++;
+                }
+                // show figures for this rental
+                result += "\t" + movie[0] + "\t" + thisAmount.ToString("0.0") + "\n";
+                totalAmount += thisAmount;
+            }
+
+            // add footer lines
+            result += "You owed " + totalAmount.ToString("0.0") + "\n";
+            result += "You earned " + frequentRenterPoints + " frequent renter points\n";
+
+            _out.Write(result);
+        }
+    }
+}
---
+++ emerging-design/CommandLineVideoStore/CommandLineVideoStore/MainTest.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace CommandLineVideoStore
+{
+    [TestFixture]
+    public class MainTest
+    {
+        [Test]
+        public void Run()
+        {
+            const string consoleInput = "John Doe\n" +
+                                        "0 1\n" +
+                                        "2 3\n" +
+                                        "6 1\n" +
+                                        "9 1\n" +
+                                        "10 2\n" +
+                                        "13 5\n" +
+                                        "\n";
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(consoleInput);
+            var inputStream = new MemoryStream(inputBytes);
+            var inputStreamReader = new StreamReader(inputStream);
+            var outputStringWriter = new StringWriter { NewLine = "\n" };
+
+            new MainClass(inputStreamReader, outputStringWriter).Run();
+
+            Assert.AreEqual("0: The Shawshank Redemption (1994)\n" +
+                            "1: The Godfather (1972)\n" +
+                            "2: The Godfather: Part II (1974)\n" +
+                            "3: The Dark Knight (2008)\n" +
+                            "4: Pulp Fiction (1994)\n" +
+                            "5: The Good, the Bad and the Ugly (1966)\n" +
+                            "6: The Lord of the Rings: The Return of the King (2003)\n" +
+                            "7: Fight Club (1999)\n" +
+                            "8: The Lord of the Rings: The Fellowship of the Ring (2001)\n" +
+                            "9: Interstellar (2014)\n" +
+                            "10: Whiplash (2014)\n" +
+                            "11: Birdman (2014)\n" +
+                            "12: Up (2009)\n" +
+                            "13: WALL·E (2008)\n" +
+                            "Enter customer name: Choose movie by number followed by rental days, just ENTER for bill:\n" +
+                            "Rental Record for John Doe\n" +
+                            "\tThe Shawshank Redemption (1994)\t2.0\n" +
+                            "\tThe Godfather: Part II (1974)\t3.5\n" +
+                            "\tThe Lord of the Rings: The Return of the King (2003)\t1.5\n" +
+                            "\tInterstellar (2014)\t3.0\n" +
+                            "\tWhiplash (2014)\t6.0\n" +
+                            "\tWALL·E (2008)\t4.5\n" +
+                            "You owed 20.5\n" +
+                            "You earned 7 frequent renter points\n",
+                outputStringWriter.ToString());
+        }
+    }
+}
